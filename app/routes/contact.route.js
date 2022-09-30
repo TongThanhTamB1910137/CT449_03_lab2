d80020b04bc4e796c645a33827c4071a2a9e0f8e
TongThanhTam_B1910137 <tamB1910137@student.ctu.edu.vn>
2022-09-30 08:08:24 +0700

    Dinh nghia cac route cho tai nguyen contact

---
+++ app/routes/contact.route.js
@@ -0,0 +1,19 @@
+let express = require("express");
+let contacts = require("../controllers/contact.controller");
+
+let router = express.Router();
+
+router.route("/").
+    get(contacts.findAll).
+    post(contacts.create).
+    delete(contacts.deleteAll);
+
+
+router.route("/favorite").get(contacts.findAllFavorite);
+
+router.route("/:id").
+    get(contacts.findOne).
+    put(contacts.update).
+    delete(contacts.delete);
+
+module.exports = router;
